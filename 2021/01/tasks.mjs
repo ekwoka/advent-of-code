322bcaff1ac77bbc08cc89405f97fe512a6ee9fe
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2021-12-03 10:59:33 -0800

    :sparkles: Add Past Tasks

---
+++ 2021/01/tasks.mjs
@@ -0,0 +1,32 @@
+// https://adventofcode.com/2021/day/1
+
+import input from "./input.mjs";
+
+//Task 1:
+
+function countDepth(samples) {
+  return samples.reduce(
+    ([count, prev], current) => {
+      if (current > prev) return [count + 1, current];
+      return [count, current];
+    },
+    [-1, 0]
+  )[0];
+}
+
+// Task 2:
+
+function countRollingDepth(samples) {
+  return samples.reduce(
+    ([count, previous, rolling], current) => {
+      if (rolling.length < 2) return [count, previous, [...rolling, current]];
+      rolling.push(current);
+      let newSum = rolling.reduce((a, b) => a + b);
+      if (newSum > previous) return [count + 1, newSum, rolling.slice(1)];
+      return [count, newSum, rolling.slice(1)];
+    },
+    [-1, 0, []]
+  )[0];
+}
+
+console.log(countDepth(input), countRollingDepth(input));
---
+++ 2021/02/tasks.mjs
@@ -0,0 +1,45 @@
+// https://adventofcode.com/2021/day/2
+
+import input from './input.mjs';
+
+// Task 1 Lost
+
+// Task 2:
+
+const commands = input.map((i) => {
+  let [command, value] = i.split(' ');
+  return { command, value: parseInt(value) };
+});
+
+function task2(commands) {
+  return commands.reduce(
+    ({ horizontal, depth, aim }, { command, value }) => {
+      if (command === 'forward')
+        return {
+          horizontal: horizontal + value,
+          depth: depth + aim * value,
+          aim
+        };
+      if (command === 'up')
+        return {
+          horizontal,
+          depth,
+          aim: aim - value
+        };
+      if (command === 'down')
+        return {
+          horizontal,
+          depth,
+          aim: aim + value
+        };
+      console.log('input error');
+    },
+    {
+      horizontal: 0,
+      depth: 0,
+      aim: 0
+    }
+  );
+}
+
+console.log(task2(commands));
