0b5a2a00a9d0d47b680fabaf2136eb603b121559
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2021-12-04 15:20:59 -0800

    :sparkles: New solution to Task 4
    75% faster!!!
    100% less human readable!!!

---
+++ 2021/04/tasks-faster.mjs
@@ -0,0 +1,45 @@
+// BINGO!!! this time with a hashmap and reducers to make it much much faster
+// This is far more efficient than the previous version, but much less readable by us humans
+// Other version: 30ms+
+// This version: 7ms+
+
+// Real Data
+
+import { boardsArray, numbers } from './input.mjs';
+
+const boards = Object.fromEntries(boardsArray.map((board, i) => [i, board]));
+const originalNumbers = [...numbers];
+
+const numberMap = Object.fromEntries(originalNumbers.map((number, i) => [number, i]));
+
+function findWinningBoardReducer(boards, numberMap) {
+  let [winningBoard, lastIndex] = Object.entries(boards).reduce(
+    (acc, [key, board]) => {
+      let lastRowIndex = board.reduce((lowest, row) => {
+        let index = row.reduce((highest, number) => {
+          let index = numberMap[number];
+          return index > highest ? index : highest;
+        }, 0);
+        return index < lowest ? index : lowest;
+      }, 1000);
+      let lastColIndex = board[0].reduce((lowest, row, column) => {
+        let index = board.reduce((highest, row) => {
+          let index = numberMap[row[column]];
+          return index > highest ? index : highest;
+        }, 0);
+        return index < lowest ? index : lowest;
+      }, 1000);
+      let lastIndex = lastRowIndex < lastColIndex ? lastRowIndex : lastColIndex;
+      return lastIndex < acc[1] ? [key, lastIndex] : acc;
+    },
+    [0, 1000]
+  );
+  const boardScore = boards[winningBoard].flat().reduce((acc, current) => {
+    return numberMap[current] > lastIndex ? acc + current : acc;
+  }, 0);
+  return boardScore * originalNumbers[lastIndex];
+}
+
+console.time('reduce');
+console.log(findWinningBoardReducer(boards, numberMap));
+console.timeEnd('reduce');
--- 2021/04/tasks.mjs
+++ 2021/04/tasks.mjs
@@ -7,7 +7,6 @@ import { boardsArray, numbers } from './input.mjs';
 const boards = Object.fromEntries(boardsArray.map((board, i) => [i, board]));
 const originalNumbers = [...numbers];
 
-
 // Sample Data
 
 /* const originalNumbers = [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1];
@@ -37,66 +36,66 @@ const boards = {
 }; */
 
 function findWinningBoard(boards, numbers) {
-    let selectedNumbers = [];
-    let winningBoard;
-    
-    while (!winningBoard && numbers.length) {
-        selectedNumbers.push(numbers.splice(0,1)[0]);
-        winningBoard = Object.values(boards).find((board) => checkWinningRows(board, selectedNumbers));
-        winningBoard = winningBoard || Object.values(boards).find((board) => checkWinningColumns(board, selectedNumbers));
-    }
-    return { winningBoard, selectedNumbers };
+  let selectedNumbers = [];
+  let winningBoard;
+
+  while (!winningBoard && numbers.length) {
+    selectedNumbers.push(numbers.splice(0, 1)[0]);
+    winningBoard = Object.values(boards).find((board) => checkWinningRows(board, selectedNumbers));
+    winningBoard = winningBoard || Object.values(boards).find((board) => checkWinningColumns(board, selectedNumbers));
+  }
+  return { winningBoard, selectedNumbers };
 }
 
 function checkWinningRows(board, numbers) {
-    let status = board.map(row => {
-        return row.filter(n => !numbers.includes(n)).length === 0;
-    })
-    return status.some(row => row === true)
+  let status = board.map((row) => {
+    return row.filter((n) => !numbers.includes(n)).length === 0;
+  });
+  return status.some((row) => row === true);
 }
 
 function checkWinningColumns(board, numbers) {
-    let translatedBoard = board[0].map((n, i) => board.map(row => row[i]));
-    return checkWinningRows(translatedBoard, numbers);
+  let translatedBoard = board[0].map((n, i) => board.map((row) => row[i]));
+  return checkWinningRows(translatedBoard, numbers);
 }
 
 // Get first board to win and 'score' of board when it wins
 
 function task1(boards, numbers) {
-    let { winningBoard, selectedNumbers } = findWinningBoard(boards,numbers)
-    let remaining = winningBoard.flat().filter(n => !selectedNumbers.includes(n));
-    let finalNumber = selectedNumbers.at(-1);
-    return finalNumber * remaining.reduce((acc, n) => acc + n);
+  let { winningBoard, selectedNumbers } = findWinningBoard(boards, numbers);
+  let remaining = winningBoard.flat().filter((n) => !selectedNumbers.includes(n));
+  let finalNumber = selectedNumbers.at(-1);
+  return finalNumber * remaining.reduce((acc, n) => acc + n);
 }
 
 function findLastBoard(boards, numbers) {
-    let selectedNumbers = [];
-    let remainingBoards = Object.values(boards);
+  let selectedNumbers = [];
+  let remainingBoards = Object.values(boards);
 
+  while (remainingBoards.length > 1 && numbers.length) {
+    selectedNumbers.push(numbers.splice(0, 1)[0]);
+    remainingBoards = remainingBoards.filter((board) => !checkWinningRows(board, selectedNumbers));
+    remainingBoards = remainingBoards.filter((board) => !checkWinningColumns(board, selectedNumbers));
+  }
 
-    while (remainingBoards.length > 1 && numbers.length) {
-        selectedNumbers.push(numbers.splice(0,1)[0]);
-        remainingBoards = remainingBoards.filter((board) => !checkWinningRows(board, selectedNumbers));
-        remainingBoards = remainingBoards.filter((board) => !checkWinningColumns(board, selectedNumbers));
-    }
+  while (!checkWinningRows(remainingBoards[0], selectedNumbers) && !checkWinningColumns(remainingBoards[0], selectedNumbers)) {
+    selectedNumbers.push(numbers.splice(0, 1)[0]);
+  }
 
-    while (!checkWinningRows(remainingBoards[0], selectedNumbers) && !checkWinningColumns(remainingBoards[0], selectedNumbers)) {
-        selectedNumbers.push(numbers.splice(0,1)[0]);
-    }
-
-    return { lastBoard: remainingBoards[0], selectedNumbers };
+  return { lastBoard: remainingBoards[0], selectedNumbers };
 }
 
 // Get last board to win and 'score' of board when it wins
 
 function task2(boards, numbers) {
-    let { lastBoard, selectedNumbers } = findLastBoard(boards, numbers);
-    let remaining = lastBoard.flat().filter(n => !selectedNumbers.includes(n));
-    let finalNumber = selectedNumbers.at(-1);
-    return finalNumber * remaining.reduce((acc, n) => acc + n);
+  let { lastBoard, selectedNumbers } = findLastBoard(boards, numbers);
+  let remaining = lastBoard.flat().filter((n) => !selectedNumbers.includes(n));
+  let finalNumber = selectedNumbers.at(-1);
+  return finalNumber * remaining.reduce((acc, n) => acc + n);
 }
 
-
+console.time('loop');
 console.log(task1(boards, [...originalNumbers]));
+console.timeEnd('loop');
 
-console.log(task2(boards, [...originalNumbers]));
+// console.log(task2(boards, [...originalNumbers]));
