f1241b41e7b9c37bc94227948df6036ba1002474
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-12-01 15:59:14 +0900

    :recycle: Refactors for simplicity

--- 2022/01/index.ts
+++ 2022/01/index.ts
@@ -4,13 +4,10 @@ const input = await readFile('input.txt', 'utf8');
 
 const elves: number[][] = input.split('\n\n').map((elf: string) => elf.split('\n').map(Number));
 
-const caloriesPerElf = elves.map((elf) => elf.reduce((total, meal) => total + meal));
+const caloriesPerElf = elves.map((elf) => elf.reduce((total, meal) => total + meal)).sort((a, b) => b - a);
 
-console.log('Part 1:', caloriesPerElf.sort((a, b) => a - b).at(-1));
+console.log('Part 1:', caloriesPerElf[0]);
 console.log(
   'Part 2:',
-  caloriesPerElf
-    .sort((a, b) => b - a)
-    .slice(0, 3)
-    .reduce((total, meal) => total + meal)
+  caloriesPerElf.slice(0, 3).reduce((total, meal) => total + meal)
 );
