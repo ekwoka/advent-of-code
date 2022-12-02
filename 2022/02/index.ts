e6a3d15b394555993dad2f68575464ea0a73e0cd
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-12-02 14:52:30 +0900

    :tada: Finish Day 2

---
+++ 2022/02/index.ts
@@ -0,0 +1,56 @@
+import { readFile } from 'node:fs/promises';
+
+const input: string = await readFile('input.txt', 'utf8');
+
+const typeMap = {
+  A: 1, // Rock
+  B: 2, // Paper
+  C: 3, // Scissors
+  X: 1,
+  Y: 2,
+  Z: 3
+};
+
+const resultMap = {
+  1: 'You Lose',
+  2: 'You Draw',
+  3: 'You Win'
+};
+
+const signMap = {
+  1: 'Rock',
+  2: 'Paper',
+  3: 'Scissors'
+};
+
+const rounds: number[][] = input
+  .split('\n')
+  .filter(Boolean)
+  .map((round: string) =>
+    round
+      .split(' ')
+      .filter(Boolean)
+      .map((type) => typeMap[type])
+  );
+
+const Part1 = (rounds: number[][]) => {
+  const scores = rounds.map(([opp, self]) => {
+    const result = opp - self;
+    if (result === 0) return 3 + self;
+    if (result === 1 || result === -2) return self;
+    return 6 + self;
+  });
+  return scores.reduce((a, b) => a + b);
+};
+
+const Part2 = (rounds: number[][]) => {
+  const scores = rounds.map(([opp, self]) => {
+    const resultScore = (self - 1) * 3;
+    const signScore = ((opp + 2 + (self - 2)) % 3) + 1;
+    return resultScore + signScore;
+  });
+  return scores.reduce((a, b) => a + b);
+};
+
+console.log('Part 1:', Part1(rounds));
+console.log('Part 2:', Part2(rounds));
