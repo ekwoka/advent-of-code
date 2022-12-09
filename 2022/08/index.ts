1f857b8564c3d86ac3a51a6e37884e87b3417fac
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-12-09 15:21:56 +0900

    :memo: Adds comments to 22/08

--- 2022/08/index.ts
+++ 2022/08/index.ts
@@ -1,3 +1,10 @@
+/**
+ * --- Day 8: Treetop Tree House ---
+ * Part 1: 00:11:11   1065
+ * Part 2: 00:39:28   3547
+ * This is a kind of mapping problem, just about finding, along the compass directions, the distances between trees of the same height.
+ */
+
 import { readFile } from 'node:fs/promises';
 
 const input: string = await readFile('input.txt', 'utf-8');
@@ -7,6 +14,7 @@ const grid = input
   .filter(Boolean)
   .map((line) => line.split('').filter(Boolean).map(Number));
 
+// Here we just take a coordinate (a tree), and output 4 arrays of all the other trees in each of the 4 cardinal directions.
 const getSightLines = (x: number, y: number, grid: number[][]) => {
   const horizontal = [grid[y].slice(x + 1), grid[y].slice(0, x).reverse()];
   const vertical = [
@@ -19,6 +27,7 @@ const getSightLines = (x: number, y: number, grid: number[][]) => {
   return [horizontal, vertical];
 };
 
+// Part 1 consists of identifying which of the trees have a sightline to the edge of the grid (no other trees of equal or greater height in the way)
 const part1 = (grid: number[][]) => {
   const areVisible = grid.flatMap((row, y) =>
     row.filter((tree, x) => {
@@ -31,6 +40,8 @@ const part1 = (grid: number[][]) => {
   );
   return areVisible.length;
 };
+
+// Part 2 consists of finding the distances in each direction that can be seen from a position, and multiplying them together, then returning the best score
 const part2 = (grid: number[][]) => {
   const scenicScores = grid.flatMap((row, y) =>
     row.map((tree, x) => {
