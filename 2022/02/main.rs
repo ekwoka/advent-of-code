e47e961750cc47790096828bf6eff2164fd119d3
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-04-02 15:03:03 +0400

    :recycle: AI Refactorings

--- 2022/02/main.rs
+++ 2022/02/main.rs
@@ -1,58 +1,52 @@
 #!/usr/bin/env rust-script
 use std::fs;
-use std::collections::HashMap;
 
-fn part_one(rounds: Vec<Vec<i32>>) {
-    let scores = rounds
-        .iter()
-        .map(|round| {
-            let result: i32 = (round[0] - round[1] + 3) % 3;
-            if result == 0 {
-                return 3 + round[1];
-            }
-            if result == 1 {
-                return 0 + round[1];
-            }
-            6 + round[1]
-        })
-        .reduce(|a, b| a + b)
-        .unwrap();
-    println!("Part one: {}", scores);
+fn part_one(rounds: Vec<(i32, i32)>) -> Result<i32, String> {
+    let scores = rounds.iter().fold(0, |sum, round| {
+        let result: i32 = (round.0 - round.1 + 3) % 3;
+        let bonus = match result {
+            0 => 3,
+            1 => 0,
+            2 => 6,
+            _ => 0,
+        };
+        sum + bonus + round.1
+    });
+    Ok(scores)
 }
 
-fn part_two(rounds: Vec<Vec<i32>>) {
-    let scores = rounds
-        .iter()
-        .map(|round| {
-            let result_score: i32 = (round[1] - 1) * 3;
-            let sign_score = ((round[0] + 2 + (round[1] - 2)) % 3) + 1;
-            result_score + sign_score
-        })
-        .reduce(|a, b| a + b)
-        .unwrap();
-    println!("Part two: {}", scores);
+fn part_two(rounds: Vec<(i32, i32)>) -> Result<i32, String> {
+    let scores = rounds.iter().fold(0, |sum, round| {
+        let result_score: i32 = (round.1 - 1) * 3;
+        let sign_score = ((round.0 + 2 + (round.1 - 2)) % 3) + 1;
+        sum + result_score + sign_score
+    });
+    Ok(scores)
 }
 
-fn main() {
-    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-    let type_map = HashMap::from([
-        ('A', 1),
-        ('B', 2),
-        ('C', 3),
-        ('X', 1),
-        ('Y', 2),
-        ('Z', 3),
-    ]);
-    let rounds: Vec<Vec<i32>> = contents
+fn type_to_score(ch: &char) -> i32 {
+    match ch {
+        'A' | 'X' => 1,
+        'B' | 'Y' => 2,
+        'C' | 'Z' => 3,
+        _ => 0,
+    }
+}
+
+fn main() -> Result<(), String> {
+    let contents = fs::read_to_string("input.txt").map_err(|_| "No file")?;
+
+    let rounds: Vec<(i32, i32)> = contents
         .lines()
-        .map(|line|
-            line
-                .trim()
-                .split(' ')
-                .map(|item| type_map[&item.chars().next().unwrap()])
-                .collect::<Vec<i32>>()
-        )
+        .map(|line| {
+            let mut chars = line.trim().chars();
+            let first = type_to_score(&chars.next().unwrap());
+            chars.next();
+            let second = type_to_score(&chars.next().unwrap());
+            (first, second)
+        })
         .collect();
-    part_one(rounds.clone());
-    part_two(rounds);
+    println!("Part one: {}", part_one(rounds.clone())?);
+    println!("Part two: {}", part_two(rounds)?);
+    Ok(())
 }
