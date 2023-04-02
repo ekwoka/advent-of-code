b315ce24d77eba47a619c2c21e431a2341086f42
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-04-02 14:07:35 +0400

    :rotating_light: Moves into Main fn

--- 2022/01/main.rs
+++ 2022/01/main.rs
@@ -1,25 +1,29 @@
 #!/usr/bin/env rust-script
 use std::fs;
 
-let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-let mut elves = contents
-    .split("\n\n")
-    .map(|elf|
-        elf
-            .lines()
-            .map(|line| line.parse::<i32>().unwrap())
-            .sum()
-    )
-    .collect::<Vec<i32>>();
-elves.sort();
-let sorted = elves.into_iter().rev().collect::<Vec<i32>>();
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+    let mut elves = contents
+        .split("\n\n")
+        .map(|elf|
+            elf
+                .lines()
+                .map(|line| line.parse::<i32>().unwrap())
+                .sum()
+        )
+        .collect::<Vec<i32>>();
+    elves.sort();
+    let sorted = elves.into_iter().rev().collect::<Vec<i32>>();
 
-println!("Part 1: {:?}", sorted.first().expect("No elves found"));
-println!(
-    "Part 2: {:?}",
-    sorted
-        .get(0..3)
-        .expect("Not enough elves found")
-        .into_iter()
-        .sum::<i32>()
-);
+    println!("Part 1: {:?}", sorted.first().expect("No elves found"));
+    println!(
+        "Part 2: {:?}",
+        sorted
+            .get(0..3)
+            .expect("Not enough elves found")
+            .into_iter()
+            .sum::<i32>()
+    );
+}
+
+main();
