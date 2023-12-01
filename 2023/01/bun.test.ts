6a637f3efae0db063d762634c8349a8a655a20c7
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-12-01 18:30:07 +0400

    :white_check_mark: Tests Rust Code in Bun

--- 2023/01/bun.test.ts
+++ 2023/01/bun.test.ts
@@ -1,5 +1,8 @@
 import { partOne, partOneStream, partTwo, partTwoStream } from '.';
+import { part_one, part_two } from './main.rs';
 import { AOCInput, getInput } from '../../utils';
+
+export const toStr = (input: string) => new TextEncoder().encode(input);
 
 describe('2023 Day 1', async () => {
   const input = await getInput(2023, 1);
@@ -47,5 +50,28 @@ describe('Character Stream', async () => {
   });
   it('Passes Part 2', () => {
     expect(partTwoStream(input)).toBe(54845);
+  });
+});
+describe('in Rust', async () => {
+  const input = await getInput(2023, 1);
+  it('Passes Part 1 Test', () => {
+    expect(part_one(toStr('1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet'))).toBe(
+      142,
+    );
+  });
+  it('Passes Part 2 Test', () => {
+    expect(
+      part_two(
+        toStr(
+          'two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen',
+        ),
+      ),
+    ).toBe(281);
+  });
+  it('Passes Part 1', () => {
+    expect(part_one(toStr(input.toString()))).toBe(55090);
+  });
+  it('Passes Part 2', () => {
+    expect(part_two(toStr(input.toString()))).toBe(54845);
   });
 });
