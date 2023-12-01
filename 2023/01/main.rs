7effa642286a0f8c9889e7d1bf6dba7e1cfc9dd5
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-12-01 13:52:41 +0400

    :memo: Adds Documentation

--- 2023/01/main.rs
+++ 2023/01/main.rs
@@ -1,5 +1,18 @@
 #!/usr/bin/env rust-script
+/**
+ * --- Day 1: Trebuchet?! ---
+ * Part 1: 00:46:00  12041
+ * Part 2: 01:32:38   9502
+ * A fairly simple problem to start with. Most of my issues (on top of starting late) were from lack of familiarity with Rust.
+ * Things got especially messy in part two when the logic wasn't quite as simple
+ */
 use std::fs;
+
+/*
+ * Part One:
+ * We need to find the numbers hidden within the strings, and sum them together.
+ * In this part, we only need to find real digits (1-9, no 0), take the first and the last, put them together as the two digits of a single number, and sum them.
+ */
 
 fn isolate_number(line: String) -> i32 {
     let mut chars = line.chars().filter(|ch| ch.is_numeric());
@@ -16,6 +29,15 @@ fn part_one(contents: &str) -> i32 {
         .map(isolate_number)
         .sum::<i32>()
 }
+
+/*
+ * Part Two:
+ * Here we discover that the strings also contain the spelled out words of numbers, and these need to also be handled!
+ * It's still find the first and last, but now those can be words or digits.
+ * My first method that solved it involved looping through all the words and finding the first and last indexes of those words, then sorting those by index and taking the first and last.
+ * This is a cleaned up version that replaces the words with digits (preservering the characters since there are words like "one" and "two" that share characters)
+ * Then from there it's the same as part one.
+ */
 
 const NUMBER_PATTERNS: [&str; 10] = [
     "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
@@ -54,6 +76,7 @@ fn main() {
     println!("Part Two: {}", part_two(&input));
 }
 
+/* Tests to assert the code works for the sample code */
 fn part_one_example() -> i32 {
     let contents = "1abc2
 pqr3stu8vwx
