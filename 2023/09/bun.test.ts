e582429c027fb59f5044d32b5afdd518b52ee707
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-12-09 10:12:29 +0400

    :tada: Solves 2023 day 9 (#11)

---
+++ 2023/09/bun.test.ts
@@ -0,0 +1,25 @@
+import { AOCInput, getInput } from '../../utils';
+import { partOne, partTwo } from '.';
+
+export const toStr = (input: string) => new TextEncoder().encode(input);
+
+describe('2023 Day 9', async () => {
+  const input = await getInput(2023, 9);
+  const example = new AOCInput(
+    '0 3 6 9 12 15\n1 3 6 10 15 21\n10 13 16 21 30 45\n',
+  );
+  describe('in TypeScript', () => {
+    it('Passes Part 1 Test', () => {
+      expect(partOne(example)).toBe(114);
+    });
+    it('Passes Part 2 Test', () => {
+      expect(partTwo(example)).toBe(2);
+    });
+    it('Passes Part 1', () => {
+      expect(partOne(input)).toBe(1757008019);
+    });
+    it('Passes Part 2', () => {
+      expect(partTwo(input)).toBe(995);
+    });
+  });
+});
