1d6be1a4247e9c65004d57369af40debb9f8ba10
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-12-11 10:31:18 +0400

    :tada: Solves 2023 Day 11

---
+++ 2023/11/bun.test.ts
@@ -0,0 +1,27 @@
+import { AOCInput, getInput } from '../../utils';
+import { solveWithExpansionFactor } from '.';
+
+export const toStr = (input: string) => new TextEncoder().encode(input);
+
+describe('2023 Day 11', async () => {
+  const input = await getInput(2023, 11);
+  const example1 = new AOCInput(
+    '...#......\n.......#..\n#.........\n..........\n......#...\n.#........\n.........#\n..........\n.......#..\n#...#.....\n',
+  );
+
+  describe('in TypeScript', () => {
+    it('Passes Part 1 Test', () => {
+      expect(solveWithExpansionFactor(example1)).toBe(374);
+    });
+    it('Passes Part 2 Test', () => {
+      expect(solveWithExpansionFactor(example1, 10)).toBe(1030);
+      expect(solveWithExpansionFactor(example1, 100)).toBe(8410);
+    });
+    it('Passes Part 1', () => {
+      expect(solveWithExpansionFactor(input)).toBe(9_370_588);
+    });
+    it('Passes Part 2', () => {
+      expect(solveWithExpansionFactor(input, 1_000_000)).toBe(746_207_878_188);
+    });
+  });
+});
---
+++ 2023/12/index.ts
@@ -0,0 +1,12 @@
+import { AOCInput } from '../../utils';
+
+/**
+ * --- Day 12: ??? ---
+ */
+export const partOne = (_input: AOCInput): number => {
+  return 0;
+};
+
+export const partTwo = (_input: AOCInput): number => {
+  return 0;
+};
