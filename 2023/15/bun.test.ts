874399c14a9fa7243663f88d1cbb17fbb7e1edd6
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-12-15 19:52:38 -0800

    :tada: Solves Day 15 Part 2

--- 2023/15/bun.test.ts
+++ 2023/15/bun.test.ts
@@ -1,10 +1,10 @@
-import { AOCInput /* getInput */ } from '../../utils';
+import { AOCInput, getInput } from '../../utils';
 import { partOne, partTwo } from '.';
 
 export const toStr = (input: string) => new TextEncoder().encode(input);
 
-describe('2023 Day 15', /* async */ () => {
-  /*   const input = new AOCInput(''); */
+describe('2023 Day 15', async () => {
+  const input = await getInput(2023, 15);
   const example1 = new AOCInput(
     'rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7',
   );
@@ -13,14 +13,14 @@ describe('2023 Day 15', /* async */ () => {
     it('Passes Part 1 Test', () => {
       expect(partOne(example1)).toBe(1320);
     });
-    it.skip('Passes Part 2 Test', () => {
-      expect(partTwo(example1)).toBe(0);
+    it('Passes Part 2 Test', () => {
+      expect(partTwo(example1)).toBe(145);
     });
-    it.skip('Passes Part 1', () => {
-      expect(partOne(example1)).toBe(0);
+    it('Passes Part 1', () => {
+      expect(partOne(input)).toBe(519603);
     });
-    it.skip('Passes Part 2', () => {
-      expect(partTwo(example1)).toBe(0);
+    it('Passes Part 2', () => {
+      expect(partTwo(input)).toBe(244342);
     });
   });
 });
--- 2023/15/index.ts
+++ 2023/15/index.ts
@@ -17,6 +17,39 @@ export const partOne = (input: AOCInput): number => {
     .sum();
 };
 
-export const partTwo = (_input: AOCInput): number => {
-  return 0;
+export const partTwo = (input: AOCInput): number => {
+  return input
+    .lines()
+    .nth(0)
+    .splitBy(',')
+    .map((part) => part.split(/=|-/).filter(Boolean))
+    .fold(
+      (acc, [key, value]) => {
+        const box = hash(key);
+        (acc[box] ??= new Map<string, number>())[value ? 'set' : 'delete'](
+          key,
+          +value,
+        );
+        return acc;
+      },
+      Array(256).fill(null) as Map<string, number>[],
+    )
+    .toIter()
+    .enumerate()
+    .filter(([_, map]) => map?.size > 0)
+    .flatMap(([box, map]) =>
+      map
+        .toIter()
+        .enumerate()
+        .map(([slot, [_, value]]) => (box + 1) * (slot + 1) * value),
+    )
+    .sum();
+};
+
+const hash = (input: string): number => {
+  return input
+    .split('')
+    .toIter()
+    .map((ch) => ch.charCodeAt(0))
+    .reduce((acc, ch) => ((acc + ch) * 17) % 256, 0);
 };
