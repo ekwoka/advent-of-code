cacd45643ebf89ba69c338d386db9289440357aa
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-12-23 16:38:18 -0800

    :tada: Starts Day 20 (#21)
    * :construction: Scaffold pla
    
    * :tada: Completes 2023 Day 20

---
+++ 2023/20/bun.test.ts
@@ -0,0 +1,27 @@
+import { AOCInput, getInput } from '../../utils';
+import { partOne, partTwo } from '.';
+
+export const toStr = (input: string) => new TextEncoder().encode(input);
+
+describe('2023 Day 20', async () => {
+  const input = await getInput(2023, 20);
+  const example1 = new AOCInput(
+    'broadcaster -> a, b, c\n%a -> b\n%b -> c\n%c -> inv\n&inv -> a\n',
+  );
+  const example2 = new AOCInput(
+    'broadcaster -> a\n%a -> inv, con\n&inv -> b\n%b -> con\n&con -> output\n',
+  );
+
+  describe('in TypeScript', () => {
+    it('Passes Part 1 Test', () => {
+      expect(partOne(example1)).toBe(32000000);
+      expect(partOne(example2)).toBe(11687500);
+    });
+    it('Passes Part 1', () => {
+      expect(partOne(input)).toBe(800830848);
+    });
+    it('Passes Part 2', () => {
+      expect(partTwo(input)).toBe(244055946148853);
+    });
+  });
+});
