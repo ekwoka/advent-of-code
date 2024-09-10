1d0312d47d58108e2990a36d02e0b1d40f6071b3
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-09-10 22:04:18 +0400

    :construction: Uses Vec2

--- 2015/03/index.ts
+++ 2015/03/index.ts
@@ -1,34 +1,28 @@
 import type { AOCInput } from '../../utils';
+import { Vec2 } from '../../utils/vec';
 
+const DirectionVector = {
+  '^': new Vec2(0, 1),
+  '>': new Vec2(1, 0),
+  v: new Vec2(0, -1),
+  '<': new Vec2(-1, 0),
+};
 export const partOne = (input: AOCInput): number => {
   return (
     1 +
     input
       .chars()
-      .map((char) => {
-        switch (char) {
-          case '^':
-            return [0, 1];
-          case '>':
-            return [1, 0];
-          case 'v':
-            return [0, -1];
-          case '<':
-            return [-1, 0];
-          default:
-            return [0, 0];
-        }
-      })
+      .map<Vec2>((char) => DirectionVector[char])
       .scan(
-        ([[pos, visited]], val) => {
-          pos[0] += val[0];
-          pos[1] += val[1];
-          if (visited.has(`${pos[0]},${pos[1]}`)) return null;
-          visited.add(`${pos[0]},${pos[1]}`);
+        ([state], step) => {
+          state[0] = state[0].add(step);
+          const [pos, visited] = state;
+          if (visited.has(pos.toString())) return null;
+          visited.add(pos.toString());
           return true;
         },
-        [[0, 0], new Set<string>(['0,0'])] as [
-          [x: number, y: number],
+        [Vec2.zero(), new Set<string>([Vec2.zero().toString()])] as [
+          Vec2,
           Set<string>,
         ],
       )
@@ -42,37 +36,23 @@ export const partTwo = (input: AOCInput): number => {
     1 +
     input
       .chars()
-      .map((char) => {
-        switch (char) {
-          case '^':
-            return [0, 1];
-          case '>':
-            return [1, 0];
-          case 'v':
-            return [0, -1];
-          case '<':
-            return [-1, 0];
-          default:
-            return [0, 0];
-        }
-      })
+      .map<Vec2>((char) => DirectionVector[char])
       .scan(
-        ([state], val) => {
+        ([state], step) => {
+          state[state[3]] = state[state[3]].add(step);
           const pos = state[state[3]];
-          state[3] ^= 1;
           const visited = state[2];
-          pos[0] += val[0];
-          pos[1] += val[1];
-          if (visited.has(`${pos[0]},${pos[1]}`)) return null;
-          visited.add(`${pos[0]},${pos[1]}`);
+          state[3] ^= 1;
+          if (visited.has(pos.toString())) return null;
+          visited.add(pos.toString());
           return pos;
         },
-        [[0, 0], [0, 0], new Set<string>(['0,0']), 0] as [
-          santa: [x: number, y: number],
-          robosanta: [x: number, y: number],
-          Set<string>,
-          step: 0 | 1,
-        ],
+        [
+          Vec2.zero(),
+          Vec2.zero(),
+          new Set<string>([Vec2.zero().toString()]),
+          0,
+        ] as [santa: Vec2, robosanta: Vec2, Set<string>, step: 0 | 1],
       )
       .filter(Boolean)
       .count()
