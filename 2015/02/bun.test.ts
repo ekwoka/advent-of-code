173bc787ec5dfa774545b52e5341ab1890dc3943
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-09-10 18:24:11 +0400

    :sparkles: Completes 2015 02 (#29)

---
+++ 2015/02/bun.test.ts
@@ -0,0 +1,18 @@
+import { partOne, partTwo } from '.';
+import { AOCInput, getInput } from '../../utils';
+
+describe('2023 Day 1', async () => {
+  const input = await getInput(2015, 2);
+  it('Passes Part 1 Test', () => {
+    expect(partOne(new AOCInput('2x3x4\n1x1x10'))).toBe(101);
+  });
+  it('Passes Part 2 Test', () => {
+    expect(partTwo(new AOCInput('2x3x4\n1x1x10'))).toBe(48);
+  });
+  it('Passes Part 1', () => {
+    expect(partOne(input)).toBe(1588178);
+  });
+  it('Passes Part 2', () => {
+    expect(partTwo(input)).toBe(3783758);
+  });
+});
