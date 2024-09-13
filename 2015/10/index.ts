e2c57e1bb6a881e5de402962642109d9e6c2de3f
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-09-13 10:37:19 +0400

    :sparkles: Completes 2015 10

---
+++ 2015/10/index.ts
@@ -0,0 +1,10 @@
+import type { AOCInput } from '../../utils';
+
+const runLengthEncode = (input: string) =>
+  input.replaceAll(/((\d)\2*)/g, (match) => `${match.length}${match[0]}`);
+
+export const partOne = (input: AOCInput, repetition: number = 40): number => {
+  let speak = input.lines().filter(Boolean).nth();
+  while (repetition--) speak = runLengthEncode(speak);
+  return speak.length;
+};
