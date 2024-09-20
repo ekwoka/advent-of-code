c0764c46c856958834918a735f857d5d931325a9
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-09-20 13:52:32 +0400

    :sparkles: Completes 2020 day 15 (#40)

---
+++ 2020/15/bun.test.ts
@@ -0,0 +1,18 @@
+import { partOne } from '.';
+import { AOCInput, getInput } from '../../utils';
+
+describe('2020 Day 15', async () => {
+  const input = await getInput(2020, 15);
+  it('Passes Part 1 Test', () => {
+    expect(partOne(new AOCInput(`0,3,6`))).toBe(436);
+  });
+  it('Passes Part 1', () => {
+    expect(partOne(input)).toBe(249);
+  });
+  it('Passes Part 2 Test', () => {
+    expect(partOne(new AOCInput(`0,3,6`), 30_000_000)).toBe(175_594);
+  });
+  it('Passes Part 2', () => {
+    expect(partOne(input, 30_000_000)).toBe(41_687);
+  });
+});
