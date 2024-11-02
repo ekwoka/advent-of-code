550e2440589fe298bb8e1b335b450ceb42eead56
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-11-02 22:52:43 +0400

    :sparkles: Completes Day 5

--- 2017/04/bun.test.ts
+++ 2017/04/bun.test.ts
@@ -1,11 +1,15 @@
 import { getInputRaw } from '../../utils';
 
-const input = await getInputRaw(2017, 1);
+const input = await getInputRaw(2017, 4);
 
-describe('2017 Day 1 Rust', async () => {
+describe('2017 Day 4 Rust', async () => {
   const { part_one, part_two } = await import('./main.rs');
   it('Passes Part 1 Test', () => {
-    expect(part_one('')).toBe(0);
+    expect(
+      part_one(`aa bb cc dd ee
+aa bb cc dd aa
+aa bb cc dd aaa`),
+    ).toBe(2);
   }, 15_000);
   it('Passes Part 1', () => {
     expect(part_one(input)).toBe(0);
---
+++ 2017/05/main.rs
@@ -0,0 +1,45 @@
+//! ```cargo
+//! [dependencies]
+//! ```
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+pub fn main() {
+  console_error_panic_hook::set_once();
+}
+#[wasm_bindgen]
+pub fn part_one(input: String) -> usize {
+  let mut instructions = input.lines().map(|line| line.parse::<i32>().unwrap()).collect::<Vec<_>>();
+  let mut position = 0i32;
+  let mut count = 0;
+  loop {
+    if position >= instructions.len().try_into().unwrap() {
+      return count
+    }
+    let change = instructions[position as usize];
+    instructions[position as usize]+=1;
+    position += change;
+    count+=1;
+  }
+}
+
+#[wasm_bindgen]
+pub fn part_two(input: String) -> usize {
+  let mut instructions = input.lines().map(|line| line.parse::<i32>().unwrap()).collect::<Vec<_>>();
+  let mut position = 0i32;
+  let mut count = 0;
+  loop {
+    if position < 0 || position >= instructions.len().try_into().unwrap() {
+      return count
+    }
+    let change = instructions[position as usize];
+    if change >= 3 {
+      instructions[position as usize] -= 1;
+    } else {
+      instructions[position as usize] += 1;
+    }
+    position += change;
+    count+=1;
+  }
+}
