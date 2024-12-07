8c131e311ff1079a6ef5012a1cc81c750c128289
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-12-07 19:07:23 +0400

    :zap: Optimizes Part 2 with DFS

--- 2024/07/main.rs
+++ 2024/07/main.rs
@@ -36,32 +36,28 @@ pub fn part_one(input: String) -> u64 {
 
 #[wasm_bindgen]
 pub fn part_two(input: String) -> u64 {
-  input.lines()
-    .filter_map(|lines| lines.split_once(": "))
-    .filter(|(test,values)| {
+  let mut output = 0;
+  for (test, values) in input.lines()
+    .filter_map(|lines| lines.split_once(": ")) {
       let test = test.parse::<u64>().unwrap();
-      let nums = values.split_whitespace().filter_map(|n| n.parse::<u64>().ok()).collect::<Vec<_>>();
-      (0..1<<((nums.len()*2)-1)).map(|mask| {
-        let mut result = 0u64;
-        for (idx,num) in nums.iter().enumerate() {
-          if idx == 0 {
-            result += num;
-          } else {
-            match (mask >> nums.len()) & (1 << (idx-1)) {
-              0 => match mask & (1 << (idx-1)) {
-                  0 => result += num,
-                  _ => result *= num
-                },
-              _ => result = format!("{}{}", result, num).parse::<u64>().unwrap()
-            }
-          }
-          if result > test {
-            break
-          }
+      let mut nums = values.split_whitespace()
+        .filter_map(|n| n.parse::<u64>().ok())
+        .rev()
+        .collect::<Vec<_>>();
+
+      let mut stack = vec![(nums.pop().unwrap(), nums.clone())];
+      while let Some((value, mut remaining)) = stack.pop() {
+        if value > test {
+          continue;
+        } else if let Some(next) = remaining.pop() {
+             stack.push((format!("{}{}", value, next).parse::<u64>().unwrap(), remaining.clone()));
+             stack.push((value + next, remaining.clone()));
+             stack.push((value * next, remaining.clone()));
+        } else if value == test {
+          output += test;
+          break;
         }
-        result
-      }).any(|result| result == test)
-    })
-    .filter_map( |(test,_)| test.parse::<u64>().ok())
-    .sum()
+      }
+    }
+    output
 }
