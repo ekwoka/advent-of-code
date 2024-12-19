87f69825b2974bc4d3eea3a7f9e7c15d106add89
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-12-19 07:38:16 -0800

    :sparkles: Completes 2024 day 16 Part 1

---
+++ 2024/16/bun.test.ts
@@ -0,0 +1,36 @@
+import { getInputRaw } from '../../utils';
+
+const input = await getInputRaw(2024, 16);
+
+const sample = `#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################`;
+describe('2024 Day 16 Rust', async () => {
+  const { part_one, part_two } = await import('./main.rs');
+  it('Passes Part 1 Test', () => {
+    expect(part_one(sample)).toBe(11_048);
+  }, 15_000);
+  it('Passes Part 1', () => {
+    expect(part_one(input)).toBe(102_488);
+  }, 15_000);
+  it.skip('Passes Part 2 Test', () => {
+    expect(part_two(sample)).toBe(0);
+  }, 15_000);
+  it.skip('Passes Part 2', () => {
+    expect(part_two(input)).toBe(0);
+  }, 15_000);
+});
