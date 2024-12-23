801c4896f1a04ea347e8d2e5bab19c621de6b6cc
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-12-22 21:31:58 -0800

    :sparkles: Completes 2024 Day 18 (#102)
    * :sparkles: Completes Part 1
    
    * :sparkles: Completes Part 2

---
+++ 2024/18/bun.test.ts
@@ -0,0 +1,44 @@
+import { getInputRaw } from '../../utils';
+
+const input = await getInputRaw(2024, 18);
+
+const sample = `5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0`;
+describe('2024 Day 18 Rust', async () => {
+  const { part_one, part_two } = await import('./main.rs');
+  it('Passes Part 1 Test', () => {
+    expect(part_one(sample, 6, 12)).toBe(22);
+  }, 15_000);
+  it('Passes Part 1', () => {
+    expect(part_one(input, 70, 1024)).toBe(306);
+  }, 15_000);
+  it('Passes Part 2 Test', () => {
+    expect(part_two(sample, 6)).toBe('6,1');
+  }, 15_000);
+  it('Passes Part 2', () => {
+    expect(part_two(input, 70)).toBe('38,63');
+  }, 15_000);
+});
---
+++ 2024/18/main.rs
@@ -0,0 +1,73 @@
+//! ```cargo
+//! [dependencies]
+//! ```
+#![feature(test)]
+use wasm_bindgen::prelude::*;
+use std::collections::{HashSet, VecDeque};
+
+#[wasm_bindgen(start)]
+pub fn main() {
+  console_error_panic_hook::set_once();
+}
+#[wasm_bindgen]
+pub fn part_one(input: &str, max: usize, bytes: usize) -> usize {
+  let memory = input.lines()
+    .take(bytes)
+    .map(|line| {
+      let mut coords = line.split(",").filter_map(|n| n.parse::<usize>().ok());
+      (coords.next().unwrap(), coords.next().unwrap())
+    }).collect::<HashSet<_>>();
+  let mut queue = VecDeque::<(usize, (usize, usize))>::new();
+  queue.push_back((0, (0, 0)));
+  let mut visited = HashSet::<(usize, usize)>::new();
+  while let Some((steps, (x, y))) = queue.pop_front() {
+    if memory.contains(&(x, y)) || visited.contains(&(x, y)) {
+      continue;
+    } else {
+      visited.insert((x, y));
+    }
+    if x == max && y == max {
+      return steps;
+    }
+
+    if x > 0 {
+      queue.push_back((steps+1, (x-1, y)));
+    }
+    if x < max {
+      queue.push_back((steps+1, (x+1, y)));
+    }
+    if y > 0 {
+      queue.push_back((steps+1, (x, y-1)));
+    }
+    if y < max {
+      queue.push_back((steps+1, (x, y+1)));
+    }
+  }
+  0
+}
+
+#[wasm_bindgen]
+pub fn part_two(input: &str, max: usize) -> String {
+  let bytes = input.lines().collect::<Vec<_>>();
+  let blocking_byte = (0..bytes.len()).filter(|idx|
+    part_one(input, max, idx+1) == 0
+  ).nth(0).unwrap();
+  bytes[blocking_byte].to_string()
+}
+
+#[cfg(test)]
+mod tests {
+  extern crate test;
+  use super::*;
+  use test::Bencher;
+    #[bench]
+    fn part_one_bench(b: &mut Bencher) {
+        let input = include_str!("../../utils/.cache/2024-18.txt").trim();
+        b.iter(move || assert_eq!(part_one(input, 70, 1024), 306));
+    }
+    #[bench]
+    fn part_two_bench(b: &mut Bencher) {
+        let input = include_str!("../../utils/.cache/2024-18.txt").trim();
+        b.iter(move || assert_eq!(part_two(input, 70), "38,63"));
+    }
+}
