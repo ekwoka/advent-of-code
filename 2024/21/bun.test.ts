f5667a44aaa76fcefc908fa6e3203266a1446832
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-12-26 17:44:52 -0800

    :sparkles: Completes Part 1

--- 2024/21/bun.test.ts
+++ 2024/21/bun.test.ts
@@ -13,10 +13,7 @@ describe('2024 Day 21 Rust', async () => {
     expect(part_one(sample)).toBe(126_384);
   }, 15_000);
   it('Passes Part 1', () => {
-    console.log(part_one(input));
-    expect(part_one(input)).toBeLessThan(143_536);
-    expect(part_one(input)).toBeGreaterThan(136_100);
-    expect(part_one(input)).toBe(0);
+    expect(part_one(input)).toBe(136_780);
   }, 15_000);
   it.skip('Passes Part 2 Test', () => {
     expect(part_two(sample)).toBe(0);
@@ -27,16 +24,16 @@ describe('2024 Day 21 Rust', async () => {
 });
 
 // 029A: <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
-// ----: <vA<AA>>^AvAA<^A>Av<<A>>^AvA^A<vA>^Av<<A>^A>AAvA^Av<<A>A>^AAAvA<^A>A
+// ----: <vA<AA>>^AvAA<^A>Av<<A>>^AvA^A<vA^>Av<<A>^A>AAvA^Av<<A>A^>AAA<Av>A^A
 
 // 980A: <v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A
-// ----: v<<A>>^AAAvA^A<vA<AA>>^AvAA<^A>Av<<A>A>^AAAvA<^A>A<vA>^A<A>A
+// ----: v<<A>>^AAAvA^A<vA<AA>>^AvAA<^A>Av<<A>A^>AAA<Av>A^A<vA^>A<A>A
 
 // 179A: <v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
-// ----: v<<A>>^A<vA<A>>^AAvAA<^A>Av<<A>>^AAvA^A<vA>^AA<A>Av<<A>A>^AAAvA<^A>A
+// ----: v<<A>>^A<vA<A>>^AAvAA<^A>Av<<A>>^AAvA^A<vA^>AA<A>Av<<A>A^>AAA<Av>A^A
 
 // 456A: <v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A
-// ----: v<<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>Av<<A>A>^AAvA<^A>A
+// ----: v<<A>>^AA<vA<A>>^AAvAA<^A>A<vA^>A<A>A<vA^>A<A>Av<<A>A^>AA<Av>A^A
 
 // 379A: <v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
-// ----: v<<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>Av<<A>A>^AAAvA<^A>A
+// ----: v<<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA^>AA<A>Av<<A>A^>AAA<Av>A^A
--- 2024/21/main.rs
+++ 2024/21/main.rs
@@ -28,125 +28,191 @@ impl Action {
 }
 
 struct NumericPad {
-  position: Vec2
+  button: char
 }
 
 impl NumericPad {
   fn new() -> Self {
     Self {
-      position: Vec2::new(2, 0)
+      button: 'A'
     }
   }
   fn goto(&mut self, target: char) -> Vec<char> {
-    let dest = match target {
-      '0' => Vec2::new(1, 0),
-      'A' => Vec2::new(2, 0),
-      '1' => Vec2::new(0, 1),
-      '2' => Vec2::new(1, 1),
-      '3' => Vec2::new(2, 1),
-      '4' => Vec2::new(0, 2),
-      '5' => Vec2::new(1, 2),
-      '6' => Vec2::new(2, 2),
-      '7' => Vec2::new(0, 3),
-      '8' => Vec2::new(1, 3),
-      '9' => Vec2::new(2, 3),
-      _ => todo!()
+    let movements = match (self.button, target) {
+      ('A', 'A') => "A",
+      ('A', '0') => "<A",
+      ('A', '1') => "^<<A",
+      ('A', '2') => "<^A",
+      ('A', '3') => "^A",
+      ('A', '4') => "^^<<A",
+      ('A', '5') => "<^^A",
+      ('A', '6') => "^^A",
+      ('A', '7') => "^^^<<A",
+      ('A', '8') => "<^^^A",
+      ('A', '9') => "^^^A",
+      ('0', 'A') => ">A",
+      ('0', '0') => "A",
+      ('0', '1') => "^<A",
+      ('0', '2') => "^A",
+      ('0', '3') => ">^A",
+      ('0', '4') => "^^<A",
+      ('0', '5') => "^^A",
+      ('0', '6') => ">^^A",
+      ('0', '7') => "^^^<A",
+      ('0', '8') => "^^^A",
+      ('0', '9') => ">^^^A",
+      ('1', 'A') => ">>vA",
+      ('1', '0') => ">vA",
+      ('1', '1') => "A",
+      ('1', '2') => ">A",
+      ('1', '3') => ">>A",
+      ('1', '4') => "^A",
+      ('1', '5') => ">^A",
+      ('1', '6') => ">>^A",
+      ('1', '7') => "^^A",
+      ('1', '8') => ">^^A",
+      ('1', '9') => ">>^^A",
+      ('2', 'A') => "v>A",
+      ('2', '0') => "vA",
+      ('2', '1') => "<A",
+      ('2', '2') => "A",
+      ('2', '3') => ">A",
+      ('2', '4') => "<^A",
+      ('2', '5') => "^A",
+      ('2', '6') => ">^A",
+      ('2', '7') => "<^^A",
+      ('2', '8') => "^^A",
+      ('2', '9') => ">^^A",
+      ('3', 'A') => "vA",
+      ('3', '0') => "<vA",
+      ('3', '1') => "<<A",
+      ('3', '2') => "<A",
+      ('3', '3') => "A",
+      ('3', '4') => "<<^A",
+      ('3', '5') => "<^A",
+      ('3', '6') => "^A",
+      ('3', '7') => "<<^^A",
+      ('3', '8') => "<^^A",
+      ('3', '9') => "^^A",
+      ('4', 'A') => ">>vvA",
+      ('4', '0') => ">vvA",
+      ('4', '1') => "vA",
+      ('4', '2') => "v>A",
+      ('4', '3') => "v>>A",
+      ('4', '4') => "A",
+      ('4', '5') => ">A",
+      ('4', '6') => ">>A",
+      ('4', '7') => "^A",
+      ('4', '8') => ">^A",
+      ('4', '9') => ">>^A",
+      ('5', 'A') => "vv>A",
+      ('5', '0') => "vvA",
+      ('5', '1') => "<vA",
+      ('5', '2') => "vA",
+      ('5', '3') => "v>A",
+      ('5', '4') => "<A",
+      ('5', '5') => "A",
+      ('5', '6') => ">A",
+      ('5', '7') => "<^A",
+      ('5', '8') => "^A",
+      ('5', '9') => ">^A",
+      ('6', 'A') => "vvA",
+      ('6', '0') => "<vvA",
+      ('6', '1') => "<<vA",
+      ('6', '2') => "<vA",
+      ('6', '3') => "vA",
+      ('6', '4') => "<<A",
+      ('6', '5') => "<A",
+      ('6', '6') => "A",
+      ('6', '7') => "<<^A",
+      ('6', '8') => "<^A",
+      ('6', '9') => "^A",
+      ('7', 'A') => ">>vvvA",
+      ('7', '0') => ">vvvA",
+      ('7', '1') => "vvA",
+      ('7', '2') => "vv>A",
+      ('7', '3') => "vv>>A",
+      ('7', '4') => "vA",
+      ('7', '5') => "v>A",
+      ('7', '6') => "v>>A",
+      ('7', '7') => "A",
+      ('7', '8') => ">A",
+      ('7', '9') => ">>A",
+      ('8', 'A') => "vvv>A",
+      ('8', '0') => "vvvA",
+      ('8', '1') => "<vvA",
+      ('8', '2') => "vvA",
+      ('8', '3') => "vv>A",
+      ('8', '4') => "<vA",
+      ('8', '5') => "vA",
+      ('8', '6') => "v>A",
+      ('8', '7') => "<A",
+      ('8', '8') => "A",
+      ('8', '9') => ">A",
+      ('9', 'A') => "vvvA",
+      ('9', '0') => "<vvvA",
+      ('9', '1') => "<<vvA",
+      ('9', '2') => "<vvA",
+      ('9', '3') => "vvA",
+      ('9', '4') => "<<vA",
+      ('9', '5') => "<vA",
+      ('9', '6') => "vA",
+      ('9', '7') => "<<A",
+      ('9', '8') => "<A",
+      ('9', '9') => "A",
+      _ => unreachable!()
     };
-    let diff = dest - self.position;
-
-    let mut output: Vec<char> = vec![];
-    if (dest.x > 0 || self.position.y > 0) && diff.x < 0 {
-      for _ in 0..diff.x.abs() {
-        output.push('<');
-      }
-    }
-    if diff.y < 0 {
-      for _ in 0..diff.y.abs() {
-        output.push('v');
-      }
-    }
-        if diff.y > 0 {
-      for _ in 0..diff.y {
-        output.push('^');
-      }
-    }
-    if diff.x > 0 {
-      for _ in 0..diff.x {
-        output.push('>');
-      }
-    }
-    if dest.x == 0 && self.position.y == 0 && diff.x < 0 {
-      for _ in 0..diff.x.abs() {
-        output.push('<');
-      }
-    }
-    self.position = dest;
-    output.push('A');
-    output
+    self.button = target;
+    movements.chars().collect::<Vec<_>>()
   }
 }
 
 struct DirectionalPad {
-  position: Vec2
+  button: char
 }
 
 impl DirectionalPad {
   fn new() -> Self {
     Self {
-      position: Vec2::new(2, 1)
+      button: 'A'
     }
   }
   fn goto(&mut self, target: char) -> Vec<char> {
-    let dest = match target {
-      '<' => Vec2::new(0, 0),
-      'v' => Vec2::new(1, 0),
-      '>' => Vec2::new(2, 0),
-      '^' => Vec2::new(1, 1),
-      'A' => Vec2::new(2, 1),
-      _ => todo!()
+    let movements = match (self.button, target) {
+      ('A', 'A') => "A",
+      ('A', '^') => "<A",
+      ('A', '<') => "v<<A",
+      ('A', 'v') => "<vA",
+      ('A', '>') => "vA",
+      ('^', 'A') => ">A",
+      ('^', '^') => "A",
+      ('^', '<') => "v<A",
+      ('^', 'v') => "vA",
+      ('^', '>') => "v>A",
+      ('<', 'A') => ">>^A",
+      ('<', '^') => ">^A",
+      ('<', '<') => "A",
+      ('<', 'v') => ">A",
+      ('<', '>') => ">>A",
+      ('v', 'A') => "^>A",
+      ('v', '^') => "^A",
+      ('v', '<') => "<A",
+      ('v', 'v') => "A",
+      ('v', '>') => ">A",
+      ('>', 'A') => "^A",
+      ('>', '^') => "<^A",
+      ('>', '<') => "<<A",
+      ('>', 'v') => "<A",
+      ('>', '>') => "A",
+      _ => unreachable!()
     };
-    let diff = dest - self.position;
-
-    let mut output: Vec<char> = vec![];
-
-    if dest.x != 0 && diff.x < 0 {
-      for _ in 0..diff.x.abs() {
-        output.push('<');
-      }
-    }
-    if self.position.x != 0 && diff.y > 0 {
-      for _ in 0..diff.y {
-        output.push('^');
-      }
-    }
-    if diff.x > 0 {
-      for _ in 0..diff.x {
-        output.push('>');
-      }
-    }
-    if self.position.x == 0 && diff.y > 0 {
-      for _ in 0..diff.y {
-        output.push('^');
-      }
-    }
-    if diff.y < 0 {
-      for _ in 0..diff.y.abs() {
-        output.push('v');
-      }
-    }
-    if dest.x == 0 && diff.x < 0 {
-      for _ in 0..diff.x.abs() {
-        output.push('<');
-      }
-    }
-    self.position = dest;
-    output.push('A');
-    output
+    self.button = target;
+    movements.chars().collect::<Vec<_>>()
   }
 }
 
 #[wasm_bindgen]
-pub fn part_one(input: &str) -> Vec<String> {
+pub fn part_one(input: &str) -> usize {
   input.lines()
     .map(|code| {
       let mut numpad = NumericPad::new();
@@ -155,13 +221,13 @@ pub fn part_one(input: &str) -> Vec<String> {
       code.chars()
         .flat_map(|ch| numpad.goto(ch).into_iter())
         .flat_map(|ch| dpad_1.goto(ch).into_iter())
-        /* .flat_map(|ch| dpad_2.goto(ch).into_iter()) */
-        .collect::<String>()
-        /* .count() * code[0..3].parse::<usize>().unwrap() */
+        .flat_map(|ch| dpad_2.goto(ch).into_iter())
+        /* .collect::<String>() */
+        .count() * code[0..3].parse::<usize>().unwrap()
     })
    /*  .last().unwrap() */
-    .collect()
-   /*  .sum() */
+    /* .collect() */
+    .sum()
 }
 
 #[wasm_bindgen]
