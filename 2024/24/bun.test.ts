b09765da37e9ab11b8911381e8913c0b4a9db178
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-12-30 12:49:03 -0800

    :sparkles: Completes Part 1

---
+++ 2024/24/bun.test.ts
@@ -0,0 +1,66 @@
+import { getInputRaw } from '../../utils';
+
+const input = await getInputRaw(2024, 24);
+
+const sample = `x00: 1
+x01: 0
+x02: 1
+x03: 1
+x04: 0
+y00: 1
+y01: 1
+y02: 1
+y03: 1
+y04: 1
+
+ntg XOR fgs -> mjb
+y02 OR x01 -> tnw
+kwq OR kpj -> z05
+x00 OR x03 -> fst
+tgd XOR rvg -> z01
+vdt OR tnw -> bfw
+bfw AND frj -> z10
+ffh OR nrd -> bqk
+y00 AND y03 -> djm
+y03 OR y00 -> psh
+bqk OR frj -> z08
+tnw OR fst -> frj
+gnj AND tgd -> z11
+bfw XOR mjb -> z00
+x03 OR x00 -> vdt
+gnj AND wpb -> z02
+x04 AND y00 -> kjc
+djm OR pbm -> qhw
+nrd AND vdt -> hwm
+kjc AND fst -> rvg
+y04 OR y02 -> fgs
+y01 AND x02 -> pbm
+ntg OR kjc -> kwq
+psh XOR fgs -> tgd
+qhw XOR tgd -> z09
+pbm OR djm -> kpj
+x03 XOR y03 -> ffh
+x00 XOR y04 -> ntg
+bfw OR bqk -> z06
+nrd XOR fgs -> wpb
+frj XOR qhw -> z04
+bqk OR frj -> z07
+y03 OR x01 -> nrd
+hwm AND bqk -> z03
+tgd XOR rvg -> z12
+tnw OR pbm -> gnj`;
+describe('2024 Day 24 Rust', async () => {
+  const { part_one, part_two } = await import('./main.rs');
+  it('Passes Part 1 Test', () => {
+    expect(part_one(sample)).toBe(2024n);
+  }, 15_000);
+  it('Passes Part 1', () => {
+    expect(part_one(input)).toBe(50_411_513_338_638n);
+  }, 15_000);
+  it.skip('Passes Part 2 Test', () => {
+    expect(part_two(sample)).toBe(0);
+  }, 15_000);
+  it.skip('Passes Part 2', () => {
+    expect(part_two(input)).toBe(0);
+  }, 15_000);
+});
