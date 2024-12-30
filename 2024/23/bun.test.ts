1d04b1aa5a6c019d4c423481441947550ad6277d
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-12-29 19:58:47 -0800

    :sparkles: Completes 2024 Day 22 (#110)
    * :sparkles: Completes Part 1
    
    * :sparkles: Completes Part 2

---
+++ 2024/23/bun.test.ts
@@ -0,0 +1,51 @@
+import { getInputRaw } from '../../utils';
+
+const input = await getInputRaw(2024, 23);
+
+const sample = `kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn`;
+describe('2024 Day 23 Rust', async () => {
+  const { part_one, part_two } = await import('./main.rs');
+  it('Passes Part 1 Test', () => {
+    expect(part_one(sample)).toBe(7);
+  }, 15_000);
+  it('Passes Part 1', () => {
+    expect(part_one(input)).toBe(1_348);
+  }, 15_000);
+  it('Passes Part 2 Test', () => {
+    expect(part_two(sample)).toBe('co,de,ka,ta');
+  }, 15_000);
+  it('Passes Part 2', () => {
+    expect(part_two(input)).toBe('am,bv,ea,gh,is,iy,ml,nj,nl,no,om,tj,yv');
+  }, 15_000);
+});
